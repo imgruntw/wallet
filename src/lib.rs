b1648f1ab76920b57c5a77d180837136b33ace2b
Wladislaw Imgrunt <w.imgrunt.extern@senec.com>
2024-12-20 13:53:06 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ uniffi::setup_scaffolding!();
 #[uniffi::export]
 pub fn get_balance(address: String) -> Balance {
     let address_enc = AddressEncoder::new(NetworkPrefix::Mainnet);
-    let address_value = address_enc.parse_address_from_str(address.as_str())?;
-    let script = address_value.script()?;
+    let address_value = address_enc.parse_address_from_str(address.as_str()).expect("failed ergo_lib");
+    let script = address_value.script().expect("failed ergo_lib");
 
     Balance {
         balance_in_nano: address.len() as u64,
@@ -26,7 +26,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = get_balance("address".to_string()).balance_in_nano;
-        assert_eq!(result, 7);
+        let balance = get_balance("9grWzUzkzQaqizwa2Jr2gkLq9H6NxHJ6THzoKfu1L1sKs4tvYyn".to_string());
+        let result = balance.balance_in_nano;
+        assert_eq!(result, 51);
+        println!("{}", balance.address);
     }
 }
